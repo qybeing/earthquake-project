1992d2c79cede4e5428d0fa2d24e2b1016b9fa72
leegreatgod <2419726493@qq.com>
2023-04-21 15:03:18 +0800

    P波S波标记

--- src/views/TestChart copy.vue
+++ src/views/TestChart copy.vue
@@ -1,54 +1,208 @@
 <template>
-  <div id="myChart" style="width: 600px; height: 400px;"></div>
+  <div id="myChart" style="width: 1082px;height: 220px;"></div>
 </template>
 
 <script setup lang="ts">
 import * as echarts from 'echarts'
 import { onMounted } from 'vue'
+import { raw_datas, ts } from '../testData'
 
 onMounted(() => initChart())
 
 function initChart() {
-  const myChart = echarts.init(document.getElementById('myChart') as HTMLElement)
-  const option = {
-    xAxis: {
-      type: 'category',
-      data: ['820', '932', '901', '934', '1290', '1330', '1320']
+  const myChart2 = echarts.init(document.getElementById('myChart') as HTMLElement)
+  let threshold = 12
+  let threshold2 = 17
+  const option2 = {
+    color: ['#F8CB00'],
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: { // 坐标轴指示器，坐标轴触发有效
+        type: 'line' // 默认为直线，可选为：'line' | 'shadow'
+      }
     },
+    xAxis: [{
+      id: '2',
+      type: 'category',
+      // data: ['1', '2', '3', '4', '5', '6', '7', '8'],
+      data: ts,
+      // axisTick: {
+      //   alignWithLabel: true
+      // }
+      boundaryGap: false
+    }],
     yAxis: {
-      type: 'value'
+      type: 'value',
+      id: '2',
+      // min: 0,
+      // max: 15,
+      // interval: 3,
+      axisLabel: {
+        formatter: '{value}'
+      }
     },
     series: [{
+      id: 'aaa',
+      name: '',
       type: 'line',
-      data: [820, 932, 901, 934, 1290, 1330, 1320],
-      symbol: 'none',
-      markLine: {
+      barWidth: '65%',
+      // data: [8.1, 13.3, 1.5, 14.6, 10.8, 9.0, 9.2, 9.3],
+      data: raw_datas,
+      markLine: { // 开始标预警线
         symbol: ['none', 'none'], // 去掉箭头
-        data: [
-          {
-            name: 'p',
-            xAxis: '934'
-          },
-          {
-            name: 's',
-            xAxis: '1330'
+        itemStyle: {
+          normal: {
+            borderWidth: 1,
+            lineStyle: {
+              type: 'dash',
+              color: '#333 ',
+              width: 2
+            },
+            label: {
+              textStyle: {
+                fontSize: 16,
+                fontWeight: 'bolder'
+              },
+              label: {
+                formatter: '{b}'
+              }
+            }
           }
-        ],
-        label: {
-          formatter: '{c}'
         },
-        lineStyle: {
-          type: 'dashed',
-          color: 'red',
-          width: 2
-        },
-        // symbol: 'none',
-        draggable: true,
-        animation: false,
-        silent: true
+        data: [
+          { xAxis: threshold, name: 'P' },
+          { xAxis: threshold2, name: 'S' }
+        ]
+      },
+      itemStyle: {
+        normal: {
+          color: function (params: { data: number; }) { // 根据预警线的值 显示对应的颜色
+            // build a color map as your need.
+            const colorList = ['#c23531', '#c5bf66 ']
+            if (params.data > threshold) {
+              return colorList[0]
+            } else if (params.data < threshold) {
+              return colorList[1]
+            }
+          }
+        }
       }
     }]
   }
-  myChart.setOption(option)
+  myChart2.setOption(option2)
+  myChart2.setOption({
+    // 绘制阈值的graphic line
+    graphic: [
+      {
+        type: 'rect',
+        z: 100,
+        shape: {
+          width: 1,
+          height: 400
+          // r: 10
+        },
+        position: [myChart2.convertToPixel({ xAxisId: '2' }, threshold), 0],
+        draggable: true,
+        style: {
+          fill: 'rgba(0,0,0,0.3)',
+          stroke: 'rgba(0,0,0,0.3)',
+          lineWidth: 10
+        },
+        cursor: 'move',
+        ondrag: onPointDragging
+      },
+      {
+        type: 'rect',
+        z: 100,
+        shape: {
+          width: 1,
+          height: 400
+          // r: 10
+        },
+        position: [myChart2.convertToPixel({ xAxisId: '2' }, threshold2), 0],
+        draggable: true,
+        style: {
+          fill: 'rgba(252,80,80,0.3)',
+          stroke: 'rgba(252,80,80,0.3)',
+          lineWidth: 10
+        },
+        cursor: 'move',
+        ondrag: onPointDragging2
+      }
+    ]
+  })
+
+  function showTooltip(dataIndex: any) {
+    myChart2.dispatchAction({
+      type: 'showTip',
+      seriesIndex: 0,
+      dataIndex: dataIndex
+    })
+  }
+
+  function hideTooltip(dataIndex: any) {
+    myChart2.dispatchAction({
+      type: 'hideTip'
+    })
+  }
+
+  function onPointDragging(this: { type: string; z: number; shape: { width: number; height: number; }; position: number[]; draggable: true; style: { fill: string; stroke: string; lineWidth: number; }; cursor: string; ondrag: (position: number[]) => void; }) {
+    // 阈值变动
+    console.log('position阈值变动', this.position)
+    threshold = myChart2.convertFromPixel({ xAxisId: '2' }, this.position[0])
+    console.log('threshold: ', threshold)
+    myChart2.setOption({
+      series: [{
+        id: 'aaa',
+        markLine: {
+          data: [
+            { xAxis: threshold, name: 'P' },
+            { xAxis: threshold2, name: 'S' }
+          ],
+          symbol: ['none', 'none'], // 去掉箭头
+          itemStyle: {
+            normal: {
+              borderWidth: 1,
+              lineStyle: {
+                type: 'dash',
+                color: '#333 ',
+                width: 2
+              },
+              label: {
+                textStyle: {
+                  fontSize: 16,
+                  fontWeight: 'bolder'
+                },
+                label: {
+                  formatter: '{b}'
+                }
+              }
+            }
+          }
+        }
+      }]
+    })
+  }
+  function onPointDragging2(this: { type: string; z: number; shape: { width: number; height: number; }; position: number[]; draggable: true; style: { fill: string; stroke: string; lineWidth: number; }; cursor: string; ondrag: (position: number[]) => void; }) {
+    // 阈值变动
+    console.log('position阈值变动', this.position)
+    threshold2 = myChart2.convertFromPixel({ xAxisId: '2' }, this.position[0])
+    console.log('threshold2: ', threshold2)
+    myChart2.setOption({
+      series: [{
+        id: 'aaa',
+        markLine: {
+          data: [
+            { xAxis: threshold },
+            { xAxis: threshold2 }
+          ]
+        }
+      }]
+    })
+  }
+
+  myChart2.on('click', function (params) {
+    console.log(params)
+  })
 }
 </script>
--- src/views/TestChart.vue
+++ src/views/TestChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="myChart" style="width: 600px; height: 400px;"></div>
+  <div id="myChart" style="width: 1082px;height: 220px;background-color: white; margin: auto;"></div>
 </template>
 
 <script setup lang="ts">
@@ -7,202 +7,109 @@ import * as echarts from 'echarts'
 import { onMounted } from 'vue'
 import { raw_datas, ts } from '../testData'
 
+const ptime = '1665881699'
+const stime = '1665881743'
+
 onMounted(() => initChart())
 
 function initChart() {
-  const myChart2 = echarts.init(document.getElementById('myChart') as HTMLElement)
-  let threshold = 3
-  let threshold2 = 7
-  const option2 = {
-    color: ['#F8CB00'],
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: { // 坐标轴指示器，坐标轴触发有效
-        type: 'line' // 默认为直线，可选为：'line' | 'shadow'
-      }
-    },
-    xAxis: [{
-      id: '2',
-      type: 'category',
-      // data: ['1', '2', '3', '4', '5', '6', '7', '8'],
-      data: ts,
-      // axisTick: {
-      //   alignWithLabel: true
-      // }
-      boundaryGap: false
-    }],
-    yAxis: {
-      type: 'value',
-      id: '2',
-      // min: 0,
-      // max: 15,
-      // interval: 3,
-      axisLabel: {
-        formatter: '{value}'
-      }
-    },
-    series: [{
-      id: 'aaa',
-      name: '',
-      type: 'line',
-      barWidth: '65%',
-      // data: [8.1, 13.3, 1.5, 14.6, 10.8, 9.0, 9.2, 9.3],
-      data: raw_datas,
-      markLine: { // 开始标预警线
-        symbol: ['none', 'none'], // 去掉箭头
-        itemStyle: {
-          normal: {
-            borderWidth: 1,
+  const chart = echarts.init(document.getElementById('myChart') as HTMLElement, 'white')
+  // const chart = echarts.init(this.$refs[`echarts${props.rowId}`], 'white')
+  // 把配置和数据放这里
+  chart.setOption(
+    {
+      backgroundColor: 'white',
+      tooltip: {
+        trigger: 'axis'
+      },
+      dataZoom: [{
+        type: 'inside'
+      }],
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ts
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          id: 'aaa',
+          name: '',
+          type: 'line',
+          // barWidth: '65%',
+          // data: [8.1, 13.3, 1.5, 14.6, 10.8, 9.0, 9.2, 9.3],
+          data: raw_datas,
+          markLine: {
+            symbol: ['none', 'none'], // 去掉箭头
             lineStyle: {
-              type: 'dash',
-              color: '#333 ',
-              width: 2
+              type: 'solid',
+              color: 'rgba(164, 43, 43, 1)',
+              width: 1
             },
             label: {
+              show: true,
+              position: 'end',
+              formatter: '{b}',
               textStyle: {
-                fontSize: 16,
-                fontWeight: 'bolder'
-              },
-              label: {
-                formatter: '{b}'
+                color: 'red', // 标注线终点文字颜色
+                fontSize: 20,
+                fontWeight: 500
               }
-            }
-          }
-        },
-        data: [
-          { xAxis: threshold, name: 'P' },
-          { xAxis: threshold2, name: 'S' }
-        ]
-      },
-      itemStyle: {
-        normal: {
-          color: function (params: { data: number; }) { // 根据预警线的值 显示对应的颜色
-            // build a color map as your need.
-            const colorList = ['#c23531', '#c5bf66 ']
-            if (params.data > threshold) {
-              return colorList[0]
-            } else if (params.data < threshold) {
-              return colorList[1]
-            }
-          }
-        }
-      }
-    }]
-  }
-  myChart2.setOption(option2)
-  myChart2.setOption({
-    // 绘制阈值的graphic line
-    graphic: [
-      {
-        type: 'rect',
-        z: 100,
-        shape: {
-          width: 1,
-          height: 400
-          // r: 10
-        },
-        position: [myChart2.convertToPixel({ xAxisId: '2' }, threshold), 0],
-        draggable: true,
-        style: {
-          fill: 'rgba(0,0,0,0.3)',
-          stroke: 'rgba(0,0,0,0.3)',
-          lineWidth: 10
-        },
-        cursor: 'move',
-        ondrag: onPointDragging
-      },
-      {
-        type: 'rect',
-        z: 100,
-        shape: {
-          width: 1,
-          height: 400
-          // r: 10
-        },
-        position: [myChart2.convertToPixel({ xAxisId: '2' }, threshold2), 0],
-        draggable: true,
-        style: {
-          fill: 'rgba(252,80,80,0.3)',
-          stroke: 'rgba(252,80,80,0.3)',
-          lineWidth: 10
-        },
-        cursor: 'move',
-        ondrag: onPointDragging2
-      }
-    ]
-  })
-
-  function showTooltip(dataIndex: any) {
-    myChart2.dispatchAction({
-      type: 'showTip',
-      seriesIndex: 0,
-      dataIndex: dataIndex
-    })
-  }
-
-  function hideTooltip(dataIndex: any) {
-    myChart2.dispatchAction({
-      type: 'hideTip'
-    })
-  }
-
-  function onPointDragging(this: { type: string; z: number; shape: { width: number; height: number; }; position: number[]; draggable: true; style: { fill: string; stroke: string; lineWidth: number; }; cursor: string; ondrag: (position: number[]) => void; }) {
-    // 阈值变动
-    console.log('position阈值变动', this.position)
-    threshold = myChart2.convertFromPixel({ xAxisId: '2' }, this.position[0])
-    console.log('threshold: ', threshold)
-    myChart2.setOption({
-      series: [{
-        id: 'aaa',
-        markLine: {
-          data: [
-            { xAxis: threshold, name: 'P' },
-            { xAxis: threshold2, name: 'S' }
-          ],
-          symbol: ['none', 'none'], // 去掉箭头
-          itemStyle: {
-            normal: {
-              borderWidth: 1,
+            },
+            data: [{
+              name: 'P',
+              xAxis: ptime, // 这里设置false是隐藏不了的，可以设置为-1
               lineStyle: {
-                type: 'dash',
-                color: '#333 ',
-                width: 2
+                color: 'red'
               },
               label: {
                 textStyle: {
-                  fontSize: 16,
-                  fontWeight: 'bolder'
-                },
-                label: {
-                  formatter: '{b}'
+                  color: 'red'
+                }
+              }
+            },
+            {
+              name: 'S',
+              xAxis: stime,
+              lineStyle: {
+                color: 'blue'
+              },
+              label: {
+                textStyle: {
+                  color: 'blue'
                 }
               }
             }
+            ],
+            silent: true // 标线无点击事件
           }
-        }
-      }]
+        }]
     })
-  }
-  function onPointDragging2(this: { type: string; z: number; shape: { width: number; height: number; }; position: number[]; draggable: true; style: { fill: string; stroke: string; lineWidth: number; }; cursor: string; ondrag: (position: number[]) => void; }) {
-    // 阈值变动
-    console.log('position阈值变动', this.position)
-    threshold2 = myChart2.convertFromPixel({ xAxisId: '2' }, this.position[0])
-    console.log('threshold2: ', threshold2)
-    myChart2.setOption({
-      series: [{
-        id: 'aaa',
-        markLine: {
-          data: [
-            { xAxis: threshold },
-            { xAxis: threshold2 }
-          ]
-        }
-      }]
-    })
-  }
+  // window.onresize = function () {
+  //   // 自适应大小
+  //   chart.resize()
+  // }
 
-  myChart2.on('click', function (params) {
-    console.log(params)
+  chart.on('click', function (params) { // 用于做每个点的监听，只用点击点才能够获取想要的监听效果；
+    const data = {
+      x: params.name,
+      y: params.value
+    }
+    console.log(data)
+    alert(JSON.stringify(data))
   })
 }
 </script>
