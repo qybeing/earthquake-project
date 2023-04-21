aa42a969b3af2fe0a9cdf01890b0bca98eac4ec0
leegreatgod <2419726493@qq.com>
2023-04-21 16:05:50 +0800

    标记P波S波

--- src/views/TestChart.vue
+++ src/views/TestChart.vue
@@ -1,16 +1,56 @@
 <template>
   <div id="myChart" style="width: 1082px;height: 220px;background-color: white; margin: auto;"></div>
+
+  <el-dialog v-model="dialogFormVisible" title="标记更新" width="20%">
+    <div> 所选点位： x: {{ markData.x }} , y: {{ markData.y }}</div>
+    <div>标记为：
+      <el-radio-group v-model="radio" class="ml-4">
+        <el-radio label="p">P波起点</el-radio>
+        <el-radio label="s">S波起点</el-radio>
+      </el-radio-group>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button type="primary" @click="changeMark">
+          确认
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup lang="ts">
 import * as echarts from 'echarts'
-import { onMounted } from 'vue'
+import { onMounted, reactive, ref, watch } from 'vue'
 import { raw_datas, ts } from '../testData'
 
-const ptime = '1665881699'
-const stime = '1665881743'
+const ptime = ref('1665881699')
+const stime = ref('1665881743')
+const dialogFormVisible = ref(false)
+const radio = ref('p')
+
+const markData = reactive({ x: '0', y: '0' })
 
 onMounted(() => initChart())
+watch(() => ptime.value, () => {
+  console.log('更新ptime')
+  initChart()
+}, { deep: true })
+watch(() => stime.value, () => {
+  console.log('更新stime')
+  initChart()
+}, { deep: true })
+const changeMark = () => {
+  // store.commit('changeDownSampling', n)
+  // chooseDialog.isDownSampling = false
+  if (radio.value === 'p') {
+    ptime.value = markData.x
+  } else {
+    stime.value = markData.x
+  }
+  dialogFormVisible.value = false
+}
 
 function initChart() {
   const chart = echarts.init(document.getElementById('myChart') as HTMLElement, 'white')
@@ -71,7 +111,7 @@ function initChart() {
             },
             data: [{
               name: 'P',
-              xAxis: ptime, // 这里设置false是隐藏不了的，可以设置为-1
+              xAxis: ptime.value, // 这里设置false是隐藏不了的，可以设置为-1
               lineStyle: {
                 color: 'red'
               },
@@ -83,7 +123,7 @@ function initChart() {
             },
             {
               name: 'S',
-              xAxis: stime,
+              xAxis: stime.value,
               lineStyle: {
                 color: 'blue'
               },
@@ -108,8 +148,11 @@ function initChart() {
       x: params.name,
       y: params.value
     }
+    markData.x = params.name
+    markData.y = params.value.toString()
     console.log(data)
-    alert(JSON.stringify(data))
+    dialogFormVisible.value = true
+    // alert(JSON.stringify(data))
   })
 }
 </script>
