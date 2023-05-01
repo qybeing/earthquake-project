768ea74aee12fceafdc258f180e37c9e9e92e039
leegreatgod <2419726493@qq.com>
2023-05-01 17:12:44 +0800

    添加加载失败默认图片

--- src/components/Spectrogram.vue
+++ src/components/Spectrogram.vue
@@ -3,19 +3,19 @@
         <!-- <img :src="imgFun('test_png.png')" /> -->
         <!-- <img src="../assets/home/img/response.png" /> -->
         <!-- <img src="https://p1.ssl.qhimg.com/t0118332e529a5c2001.jpg" alt="BHE"> -->
-        <img :src="getUrl('BHE')" alt="BHE">
+        <img id="BHE" :src="getUrl('BHE')" alt="BHE" @error="errorImg($event)">
     </div>
     <div class="div1" v-if="choosedchannels.includes('BHN')">
         <!-- <img :src="imgFun('test_png.png')" /> -->
         <!-- <img src="../assets/home/img/test.png" /> -->
         <!-- <img src="../assets/home/img/response.png" /> -->
         <!-- <img src="https://p1.ssl.qhimg.com/t0118332e529a5c2001.jpg"  alt="BHN"/> -->
-        <img :src="getUrl('BHN')" alt="BHN">
+        <img id="BHN" :src="getUrl('BHN')" alt="BHN" @error="errorImg($event)">
     </div>
     <div class="div1" v-if="choosedchannels.includes('BHZ')">
         <!-- <img :src="imgFun('test_png.png')" /> -->
         <!-- <img src="https://p1.ssl.qhimg.com/t0118332e529a5c2001.jpg"  alt="BHZ"> -->
-        <img :src="getUrl('BHZ')" alt="BHZ">
+        <img id="BHZ" :src="getUrl('BHZ')" alt="BHZ" @error="errorImg($event)">
     </div>
 </template>
 
@@ -23,6 +23,7 @@
 import { computed, reactive, ref } from 'vue'
 import { useStore } from 'vuex'
 import { GlobalDataProps } from '../store'
+// import defaultImage from '../assets/home/img/response.png'
 const store = useStore<GlobalDataProps>()
 const choosedchannels = reactive(computed(() => store.state.chooseChannel))
 const info = reactive(computed(() => store.state.chooseData))
@@ -30,27 +31,20 @@ const preInfo = computed(() =>
     info.value.network + '.' + info.value.station + '.' + info.value.location + '.'
 )
 
-function getUrl(channel:string) {
+function getUrl(channel: string) {
     return preInfo.value + channel
 }
-// function imgFun(val: string) {
-//     return new URL(`../../asseets/home/img/${val}`, import.meta.url).href
-// }
+function errorImg(e) {
+    console.log('image load failed')
+    console.log(e.srcElement
+        .id)
+    // const url = '../assets/home/img/' + e.srcElement.id + '_Error.png'
+    e.srcElement.src = require('../assets/home/img/BHN_Error.png')
+    // 这一句没用，如果默认图片的路径错了还是会一直闪屏，在方法的前面加个.once只让它执行一次也没用
+    e.srcElement.onerror = null // 防止闪图
 
-// function getHistoryImage(id) {
-//   return new Request({
-// url: 'https://71y830321n.goho.co/offline_mysql_curve/get_tf_png',
-// method: 'post',
-// // params: id,
-// responseType: 'blob'
-// })
-// }
-
-// getHistoryImage({ processInstanceId: row.processInstanceId }).then(res => {
-//   const blob = new Blob([res]) // 返回的文件流数据
-//   const url = window.URL.createObjectURL(blob) // 将他转化为路径
-//   this.traceSrc = url
-// })
+    // console.log(image)
+}
 
 </script>
 <style>
@@ -64,5 +58,6 @@ function getUrl(channel:string) {
 .div1 img {
     width: 95%;
     height: 98%;
-    object-fit: fill;
-}</style>
+    object-fit: contain;
+}
+</style>
