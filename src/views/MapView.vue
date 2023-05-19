6e5aedb55dae94e446d4d8f01b57a2a516d2edf1
leegreatgod <2419726493@qq.com>
2023-05-19 11:05:30 +0800

    地图样式修改

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -52,8 +52,236 @@ const store = useStore<GlobalDataProps>()
 require('echarts/theme/macarons')
 const echartsMap = ref()
 const isopen = ref(false)
-onMounted(() => getAMap())
+let myChart: echarts.ECharts | null = null
+onMounted(() => {
+  myChart = echarts.init(echartsMap.value)
+  getAMap()
+  drawSiteData()
+  drawSignalStation()
+  drawAlarmStation()
+  drawEpicenter()
+})
 
+const option = {
+  tooltip: {
+    show: true, // 提示框
+    triggerOn: 'click', // 必须使用这种方式，因为tooltip需要有点击事件，同时移入effectScatter点区域联动
+    extraCssText: 'border:none;', // 清除tooltip自带颜色
+    // alwaysShowContent: true,//提示框不消失
+    hideDelay: 2000 // 提示框2秒后小时
+  },
+  // 加载 amap 组件
+  amap: {
+    // 中文版地图
+    lang: 'cn',
+    // 3D模式，开启此项以获得更好的渲染体验
+    viewMode: '3D',
+    // 高德地图中心经纬度
+    center: [104.114129, 37.550339],
+    // 地图缩放
+    zoom: 4,
+    // 开启鼠标缩放和平移漫游
+    roam: true,
+    // 启用resize
+    resizeEnable: true,
+    mapStyle: 'amap://styles/blue',
+    // 移动过程中实时渲染 默认为true 如数据量较大 建议置为false.
+    renderOnMoving: true,
+    // ECharts 图层的 zIndex 默认 2022
+    echartsLayerZIndex: 2022
+  },
+  series: [
+    {
+      name: 'AllStation',
+      type: 'scatter',
+      coordinateSystem: 'amap',
+      data: [
+        {
+          name: '',
+          value: [0, 0]
+        }
+      ],
+      symbol: 'triangle',
+      symbolSize: 20,
+      label: {
+        formatter: '{b}',
+        position: 'right',
+        show: false
+      },
+      itemStyle: {
+        // color: '#d1cfd4'
+        color: '#9FA19F'
+      },
+      emphasis: {
+        label: {
+          show: true
+        }
+      }
+    },
+    {
+      name: 'SignalStation',
+      type: 'scatter',
+      coordinateSystem: 'amap',
+      // data: siteData,
+      data: [],
+      symbol: 'triangle',
+      symbolSize: 20,
+      label: {
+        formatter: '{b}',
+        position: 'right',
+        show: false
+      },
+      itemStyle: {
+        color: '#00D2DE'
+      },
+      emphasis: {
+        label: {
+          show: true
+        }
+      }
+    },
+    {
+      name: 'AlarmStation',
+      type: 'effectScatter',
+      coordinateSystem: 'amap',
+      // data: siteData,
+      data: [],
+      symbol: 'triangle',
+      symbolSize: 20,
+      // rippleEffect: {
+      //       // 波纹的绘制方式，可选 'stroke' 和 'fill'
+      //       brushType: 'stroke',
+      //       scale: 2,
+      //       period: 1,
+      //       number: 2
+      //     },
+      rippleEffect: {
+        brushType: 'fill',
+        scale: 2,
+        number: 3,
+        color: 'rgba(224, 31, 31, 1)'
+      },
+      label: {
+        formatter: '{b}',
+        position: 'right',
+        show: false
+      },
+      itemStyle: {
+        color: '#00D2DE'
+      },
+      zlevel: 1
+    },
+    {
+      name: 'Epicenter',
+      type: 'effectScatter',
+      coordinateSystem: 'amap',
+      // data: siteData,
+      data: [],
+      symbol: 'circle',
+      symbolSize: 20,
+      rippleEffect: {
+        brushType: 'fill',
+        scale: 3,
+        number: 4,
+        color: 'rgba(224, 31, 31, 1)'
+      },
+      label: {
+        formatter: '{b}',
+        position: 'right',
+        show: false
+      },
+      itemStyle: {
+        color: 'red'
+      },
+      zlevel: 1
+    }
+  ],
+  animation: true
+}
+
+// 地图初始化配置
+const getAMap = () => {
+  // const option = {
+  //   tooltip: {
+  //     show: true, // 提示框
+  //     triggerOn: 'click', // 必须使用这种方式，因为tooltip需要有点击事件，同时移入effectScatter点区域联动
+  //     extraCssText: 'border:none;', // 清除tooltip自带颜色
+  //     // alwaysShowContent: true,//提示框不消失
+  //     hideDelay: 2000 // 提示框2秒后小时
+  //   },
+  //   // 加载 amap 组件
+  //   amap: {
+  //     // 中文版地图
+  //     lang: 'cn',
+  //     // 3D模式，开启此项以获得更好的渲染体验
+  //     viewMode: '3D',
+  //     // 高德地图中心经纬度
+  //     center: [104.114129, 37.550339],
+  //     // 地图缩放
+  //     zoom: 4,
+  //     // 开启鼠标缩放和平移漫游
+  //     roam: true,
+  //     // 启用resize
+  //     resizeEnable: true,
+  //     mapStyle: 'amap://styles/blue',
+  //     // 移动过程中实时渲染 默认为true 如数据量较大 建议置为false.
+  //     renderOnMoving: true,
+  //     // ECharts 图层的 zIndex 默认 2022
+  //     echartsLayerZIndex: 2022
+  //   },
+  //   animation: true
+  // }
+
+  myChart?.setOption(option)
+
+  const map = myChart?.getModel().getComponent('amap').getAMap()
+  // 设置显示卫星图
+  // const Satellite = new window.AMap.TileLayer.Satellite({
+  //   zIndex: 10
+  // })
+  // map.add(Satellite)
+  map.on('zoomend', function () {
+    const zoom = map.getZoom()
+    console.log('zoom', zoom)
+  })
+  // map.addControl(new window.AMap.Scale())
+  // map.addControl(new window.AMap.ToolBar())
+
+  if (myChart) {
+    myChart && myChart.off('click')
+    myChart.on('click', function (params) {
+      router.push('/offline/ViewChart')
+      store.dispatch('fetchViewChartData')
+    })
+    myChart.on('contextmenu', function (params) {
+      console.log('右键点击点击了！', params)
+      isopen.value = true
+    })
+  }
+}
+// 获取所有台站点位数据
+const drawSiteData = () => {
+  option.series[0].data = siteData
+  myChart?.setOption(option)
+}
+// 获取信号台站点位数据
+const drawSignalStation = () => {
+  const signalData = siteData.slice(0, 6)
+  option.series[1].data = signalData
+  myChart?.setOption(option)
+}
+// 获取报警台站点位数据
+const drawAlarmStation = () => {
+  const alarmData = siteData.slice(0, 3)
+  option.series[2].data = alarmData
+  myChart?.setOption(option)
+}
+// 获取震源地点位数据
+const drawEpicenter = () => {
+  const epicenter = warnSite
+  option.series[3].data = epicenter
+  myChart?.setOption(option)
+}
 const warnSite = [
   {
     name: ' ',
@@ -63,259 +291,6 @@ const warnSite = [
     ]
   }
 ]
-function changeSerise(option: any, zoom: any, myChart: any) {
-  let input
-  if (zoom < 6) {
-    input = [
-      {
-        name: 'pm2.5',
-        type: 'scatter',
-        // 使用高德地图坐标系
-        coordinateSystem: 'amap',
-        data: networkData,
-        symbol: 'triangle',
-        symbolSize: function () {
-          return 25
-        },
-        label: {
-          formatter: '{b}',
-          position: 'right',
-          show: false
-        },
-        itemStyle: {
-          color: '#d1cfd4'
-        },
-        emphasis: {
-          label: {
-            show: true
-          }
-        }
-      },
-      {
-        name: 'warnSite',
-        type: 'effectScatter',
-        coordinateSystem: 'amap',
-        data: warnSite,
-        symbol: 'circle',
-        symbolSize: 25,
-        // 配置绘制完成时显示特效
-        showEffectOn: 'render',
-        // 涟漪特效
-        rippleEffect: {
-          // 波纹的绘制方式，可选 'stroke' 和 'fill'
-          brushType: 'fill',
-          scale: 4,
-          period: 1,
-          number: 2
-        },
-        // 开启鼠标 hover 的提示动画效果
-        hoverAnimation: true,
-        label: {
-          formatter: '{b}',
-          position: 'right',
-          show: true
-        },
-        itemStyle: {
-          color: 'red',
-          shadowBlur: 10,
-          shadowColor: '#333'
-        },
-        zlevel: 1,
-        tooltip: {
-          show: true, // 提示框
-          triggerOn: 'contextmenu'
-        }
-      }
-    ]
-  } else {
-    input = [
-      {
-        name: 'pm2.5',
-        type: 'scatter',
-        // 使用高德地图坐标系
-        coordinateSystem: 'amap',
-        data: siteData,
-        symbol: 'triangle',
-        symbolSize: 20,
-        label: {
-          formatter: '{b}',
-          position: 'right',
-          show: false
-        },
-        itemStyle: {
-          color: '#d1cfd4'
-        },
-        emphasis: {
-          label: {
-            show: true
-          }
-        }
-      },
-      {
-        name: 'Top 5',
-        type: 'effectScatter',
-        coordinateSystem: 'amap',
-        data: siteData.slice(0, 6),
-        symbol: 'triangle',
-        symbolSize: 15,
-        // 配置绘制完成时显示特效
-        showEffectOn: 'render',
-        // 涟漪特效
-        // rippleEffect: {
-        //   // 波纹的绘制方式，可选 'stroke' 和 'fill'
-        //   brushType: 'fill',
-        //   scale: 4,
-        //   period: 1,
-        //   number: 2
-        // },
-        // 开启鼠标 hover 的提示动画效果
-        hoverAnimation: true,
-        label: {
-          formatter: '{b}',
-          position: 'right',
-          show: true
-        },
-        itemStyle: {
-          color: 'red',
-          shadowBlur: 10,
-          shadowColor: '#333'
-        },
-        zlevel: 1,
-        tooltip: {
-          show: true, // 提示框
-          triggerOn: 'contextmenu'
-        }
-      },
-      {
-        name: 'warnSite',
-        type: 'effectScatter',
-        coordinateSystem: 'amap',
-        data: warnSite,
-        symbol: 'circle',
-        symbolSize: 20,
-        // 配置绘制完成时显示特效
-        showEffectOn: 'render',
-        // 涟漪特效
-        rippleEffect: {
-          // 波纹的绘制方式，可选 'stroke' 和 'fill'
-          brushType: 'fill',
-          scale: 4,
-          period: 1,
-          number: 2
-        },
-        // 开启鼠标 hover 的提示动画效果
-        hoverAnimation: true,
-        label: {
-          formatter: '{b}',
-          position: 'right',
-          show: true
-        },
-        itemStyle: {
-          color: 'red',
-          shadowBlur: 10,
-          shadowColor: '#333'
-        },
-        zlevel: 1,
-        tooltip: {
-          show: true, // 提示框
-          triggerOn: 'contextmenu'
-        }
-      }
-
-    ]
-  }
-
-  myChart.setOption({ series: input })
-}
-
-const getAMap = () => {
-  const myChart = echarts.init(echartsMap.value)
-  let option: any
-  // eslint-disable-next-line @typescript-eslint/no-this-alias
-  // const that = this
-  // eslint-disable-next-line prefer-const
-  option = {
-    tooltip: {
-      show: true, // 提示框
-      triggerOn: 'click', // 必须使用这种方式，因为tooltip需要有点击事件，同时移入effectScatter点区域联动
-      extraCssText: 'border:none;', // 清除tooltip自带颜色
-      // alwaysShowContent: true,//提示框不消失
-      hideDelay: 2000 // 提示框2秒后小时
-    },
-    // 加载 amap 组件
-    amap: {
-      // 中文版地图
-      lang: 'cn',
-      // 3D模式，开启此项以获得更好的渲染体验
-      viewMode: '3D',
-      // 高德地图中心经纬度
-      center: [104.114129, 37.550339],
-      // 地图缩放
-      zoom: 4,
-      // 开启鼠标缩放和平移漫游
-      roam: true,
-      // 启用resize
-      resizeEnable: true,
-      mapStyle: 'amap://styles/blue',
-      // 移动过程中实时渲染 默认为true 如数据量较大 建议置为false.
-      renderOnMoving: true,
-      // ECharts 图层的 zIndex 默认 2022
-      echartsLayerZIndex: 2022
-    },
-    series: [
-      {
-        name: 'pm2.5',
-        type: 'scatter',
-        // 使用高德地图坐标系
-        coordinateSystem: 'amap',
-        data: networkData,
-        symbol: 'triangle',
-        symbolSize: function () {
-          return 25
-        },
-        label: {
-          formatter: '{b}',
-          position: 'right',
-          show: false
-        },
-        itemStyle: {
-          color: 'yellow'
-        },
-        emphasis: {
-          label: {
-            show: true
-          }
-        }
-      }
-    ],
-    animation: true
-  }
-
-  option && myChart.setOption(option)
-
-  const map = myChart.getModel().getComponent('amap').getAMap()
-  // 设置显示卫星图
-  const Satellite = new window.AMap.TileLayer.Satellite({
-    zIndex: 10
-  })
-  map.add(Satellite)
-  map.on('zoomend', function () {
-    const zoom = map.getZoom()
-    console.log('zoom', zoom)
-    changeSerise(option, zoom, myChart)
-  })
-
-  myChart.off('click')
-  myChart.on('click', function (params) {
-    router.push('/offline/ViewChart')
-    store.dispatch('fetchViewChartData')
-  })
-  myChart.on('contextmenu', function (params) {
-    console.log('右键点击点击了！', params)
-    isopen.value = true
-  })
-}
-
 const curveData = {
   id: 'XJ.AHQ.00.BHN',
   longitude: 116.2164,
